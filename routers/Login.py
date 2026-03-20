adbf0c1027abf68c7e1516a227e5fe0d4b0a5338
Francis86-maker <francismukabya@gmail.com>
2026-03-20 08:09:30 +0300

    Update Login.py

--- routers/Login.py
+++ routers/Login.py
@@ -1,1 +1,28 @@
+from ..databasemodels import get_db
+from fastapi import APIRouter, Depends, HTTPException 
+from . import models 
+from ..models import User
+import bcrypt
+from jose import jwt
+from datetime import datetime , deltatime 
 
+router = APIRouter()
+
+@router.post('/login')
+def Login(request: User, db: Session=Depends(get_db)):
+  user = db.query(models.User).filter(models.User.email == request.email).first()
+  if not user:
+    raise HTTPException(status_code=404, detail="email not found")
+  password = request.password.encode("UTF-8")
+  valid_password = bcrypt.checkpw(password, user.password)
+  if not valid_password:
+    raise HTTPException(status_codd=404, detail="wrong password")
+  token_data = {
+    "user_id": user_id,
+    "role": user.role,
+    "exp": datetime.utcnow() + deltatime(hours=12)
+  }
+  token = jwt.encode(token_data, algorithm="HS256", SECRET_KEY)
+  return {
+    "message": "user logged in successfully"
+  }
