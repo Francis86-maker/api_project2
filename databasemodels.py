e1aaf4ed4313cd65891e1144f6ef4f7cff2d2db6
Francis86-maker <francismukabya@gmail.com>
2026-03-21 22:44:42 +0300

    Update databasemodels.py

--- databasemodels.py
+++ databasemodels.py
@@ -2,7 +2,7 @@ from sqlalchemy import String , Integer, Column , ForeignKey
 from database import declarative_base
 from sqlalchemy.orm import relationship , sessionmaker
 
-engine = create_engine("sqlite:///blog.db", echo=True)
+engine = create_engine("sqlite///:blog.db", echo=True)
 SessionLocal = sessionmaker(auto_commit=False, auto_Flush=False, bind=engine)
 
 Base = declarative_base()
@@ -15,7 +15,7 @@ class User(Base):
   password = Column(String)
   role = Column(String, default="user")
   owner_id = Column(Integer, ForeignKey="Items.id")
-  items = relationship("Items", back_populates="creator")
+  items = relationship("Item", back_populates="creator")
 class Item(Base):
   __tablename__ ="Items"
   id = Column(Integer, primary_key=True, unique=True)
