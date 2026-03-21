576ef97001a28546a7828b86b1679aa7f5a69693
Francis86-maker <francismukabya@gmail.com>
2026-03-21 12:33:05 +0300

    Add user ownership to item creation
    Updated create_item function to include current_user and set owner_id.

--- routers/Items.py
+++ routers/Items.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from ..models import Items
 from ..databasemodels import get_db
 from . import models 
+from auth import get_active_user
 
 router = APIRouter()
 
 @router.post('/items')
-def create_item(request: Items, db: Session=Depends(get_db)):
-  new_item = models.Items(name = request.name, price = request.price, description= request.description).first()
+def create_item(request: Items,current_user: User=Depends(get_active_user),  db: Session=Depends(get_db)):
+  new_item = models.Items(name = request.name, price = request.price, description= request.description,owner_id = current_user.id).first()
   db.add(new_item)
   db.commit()
   db.refresh(new_item)
