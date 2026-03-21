88d79be85a8128218480ce39f684c0892f814b56
Francis86-maker <francismukabya@gmail.com>
2026-03-21 22:57:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from .databasemodels import Base
 
 app = FastAPi()
-
+metadata.Base.create_all(bind=engine)
 app.include_router(User.routers)
 app.include_router(Login.routers)
-app.include_router(profile.routers)
+app.include_router(userprofile.routers)
 app.include_router(update.routers)
 app.include_router(delete.routers)
-app.include_router(delete_items.routers)
-app.include_router(profile.routers)
+app.include_router(Item.routers)
+app.include_router(Item_update.routers)
 app.include_router(admin_profile.routers)
-app.include_router(updateitem.routers)
-app.include_
+app.include_router(delete_item.routers)
+app.include_router(iteminfo.routers)
+
